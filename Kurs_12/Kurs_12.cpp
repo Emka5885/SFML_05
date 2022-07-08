08d1ce9acc7581da5e1618fa93e3c8a57d78a5bc
Emka5885 <masiak.emilia@o2.pl>
2022-07-08 20:06:29 +0200

    #1 - całość

--- Kurs_12/Kurs_12.cpp
+++ Kurs_12/Kurs_12.cpp
@@ -3,28 +3,9 @@
 
 
 //Flappy Bird
-#include <SFML/Graphics.hpp>
-
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 1024
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy Bird");
-
-    while (window.isOpen())
-    {
-        //handle events
-        
-        //update game logic
-
-        //draw
-        window.clear();
-
-        //draw SFML objects
-
-        window.display();
-    }
-
     return EXIT_SUCCESS;
 }
---
+++ Kurs_12/StateMachine.cpp
@@ -0,0 +1,56 @@
+﻿#include "StateMachine.hpp"
+
+namespace MyNamespace
+{
+	void StateMachine::AddState(StateRef newState, bool isReplacing)
+	{
+		this->_isAdding = true;
+		this->_isReplacing = isReplacing;
+
+		this->_newState = std::move(newState);   //"std::move( t )" - służy do wskazania, że ​​obiekt t może zostać „przeniesiony z”, tj. umożliwia efektywne przeniesienie zasobów z t do innego obiektu.
+	}
+
+	void StateMachine::RemoveState( )
+	{
+		this->_isRemoving = true;
+	}
+
+	void StateMachine::ProcessStateChanges( )
+	{
+		if ( this->_isRemoving && !this->_states.empty() )
+		{
+			this->_states.pop( );   //".pop()" - zdejmuje istniejący element ze szczytu stosu (usuwamy go)
+
+			if ( !this->_states.empty() )
+			{
+				this->_states.top()->Resume();
+			}
+
+			this->_isRemoving = false;
+		}
+
+		if ( this->_isAdding )
+		{
+			if ( !this->_states.empty() )
+			{
+				if ( this->_isReplacing )
+				{
+					this->_states.pop();
+				}
+				else
+				{
+					this->_states.top()->Pause();
+				}
+			}
+
+			this->_states.push( std::move( this->_newState ) );
+			this->_states.top()->Init();
+			this->_isAdding = false;
+		}
+	}
+
+	StateRef& StateMachine::GetActiveState()
+	{
+		return this->_states.top();
+	}
+}
