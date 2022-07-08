afa13a058ac5a2fd6bf330f07e4f5272aae46dc9
Emka5885 <masiak.emilia@o2.pl>
2022-07-08 18:57:02 +0200

    Dodaj pliki projektów.

---
+++ Kurs_12/Kurs_12.cpp
@@ -0,0 +1,30 @@
+﻿// Link do playlisty: https://www.youtube.com/watch?v=KNvRFEDQpOA&list=PLRtjMdoYXLf7DB3--POF9lYzaZu0poT5V&index=1
+
+
+
+//Flappy Bird
+#include <SFML/Graphics.hpp>
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy Bird");
+
+    while (window.isOpen())
+    {
+        //handle events
+        
+        //update game logic
+
+        //draw
+        window.clear();
+
+        //draw SFML objects
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
