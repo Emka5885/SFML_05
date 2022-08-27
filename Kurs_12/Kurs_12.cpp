82f19cee4dbd354a38179b416ba17781cb55625a
Emka5885 <masiak.emilia@o2.pl>
2022-08-27 13:30:58 +0200

    #28 - całość

--- Kurs_12/Kurs_12.cpp
+++ Kurs_12/Kurs_12.cpp
@@ -13,3 +13,21 @@ int main()
 
     return EXIT_SUCCESS;
 }
+
+/*
+#28   "Where To Go Next?"
+
+Po zakończeniu playlisty "Flappy Bird", można dla przećwiczenia swoich umiejętności dodać np.:
+    1 - Przycisk umożliwiający wyłączenie/włączenie dźwięków w grze
+    2 - Muzykę w tle
+    3 - Możliwości podania swojego imienia i np. zrobienia jakiejś tablicy wyników najlepszych graczy
+    4 - Przycisk powrotu do menu głównego a w nim dodanie przycisku "Exit"
+    5 - Własne zasoby, np. po osiągnięciu czegoś możemy zmienić wygląd postaci
+    6 - Przycisk "Pauza"
+    7 - Możliwość włączenia lokalnego trybu gry dwuosobowej
+    8 - Możliwość obsługi klawiatury, joysticka np. wybór czy chcesz grać myszką, klawiaturą czy joystickiem
+
+
+
+//Koniec...
+*/
